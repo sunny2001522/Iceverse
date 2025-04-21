7941559cd2934571f3c0369a4edb7943326a1178
sunny2001522 <seliya512399@gmail.com>
2025-04-21 17:51:18 +0800

    feat:service

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // 确保包含所有可能的文件类型
+    "./*.html", // 如果 HTML 文件在根目录
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#99B1F9",
+        secondary: "#FFECD2",
+        tertiary: "#FACAA8",
+      },
+    },
+  },
+  plugins: [],
+};
